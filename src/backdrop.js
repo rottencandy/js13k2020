2c34204d2478acea28a78c26145e8771b30e6dda
Mohammed Saud <md.saud020@gmail.com>
2020-09-01 23:23:34 +0530

    add backdrop

---
+++ src/backdrop.js
@@ -0,0 +1,32 @@
+import { compile, makeBuffer } from "./engine/gl";
+
+let vertexBuffer, program;
+
+let vshader = `attribute vec2 aVertexPosition;
+varying vec2 vST;
+
+void main() {
+  gl_Position = vec4(aVertexPosition.xy, 0.9, 1.0);
+  vST = (aVertexPosition+1.0)/2.0;
+}`;
+
+let fshader = `precision mediump float;
+varying vec2 vST;
+
+void main() {
+  gl_FragColor = vec4(0.7, 0.7, 0.7, 1.0);
+}`;
+
+export let init = (gl) => {
+  program = compile(gl, vshader, fshader);
+  vertexBuffer = makeBuffer(gl, gl.ARRAY_BUFFER, [-1, 1, -1, -1, 1, 1, 1, -1]);
+};
+
+export let draw = (gl) => {
+  program.use();
+
+  vertexBuffer.bind();
+  gl.vertexAttribPointer(program.attribs.vertex, 2, gl.FLOAT, false, 0, 0);
+  gl.enableVertexAttribArray(program.attribs.vertex);
+  gl.drawArrays(gl.TRIANGLE_STRIP, 0, 4);
+};
--- src/scene.js
+++ src/scene.js
@@ -1,15 +1,16 @@
 import * as Platform from "./platform";
+import * as Backdrop from "./backdrop";
 
 export let init = (gl) => {
+  Backdrop.init(gl);
   Platform.init(gl);
 };
 
 export let loadLevel = Platform.loadLevel;
 
-export let update = (delta) => {
-  return Platform.update(delta);
-};
+export let update = (delta) => Platform.update(delta);
 
 export let draw = (gl) => {
+  Backdrop.draw(gl);
   Platform.draw(gl);
 };
